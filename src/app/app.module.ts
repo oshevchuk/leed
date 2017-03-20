57149c8305d3b65de143cfc320c3239dfef529d2
Oshevchuk <megadevice83@gmail.com>
2017-03-20 14:41:00 +0200

    1411

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {routing, appRoutingProviders} from './app.routing';
+
 import { AppComponent } from './app.component';
+import {HomeComponent} from "./components/home/home.component";
+import {ProfileComponent} from "./components/profile/profile.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HomeComponent, ProfileComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+      routing
   ],
-  providers: [],
+  providers: [
+      appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
